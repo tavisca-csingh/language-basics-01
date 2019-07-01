bb246bbd22fbc3b9fb5acef7d1d97c65e02233c9
chandan singh <51908092+tavisca-csingh@users.noreply.github.com>
2019-07-01 14:41:37 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -10,6 +11,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Test("4?*47=1974", 2);
             Test("42*?7=1974", 4);
             Test("42*?47=1974", -1);
+            Test("2?*22=484", 2);
             Test("2*12?=247", -1);
             Console.ReadKey(true);
         }
@@ -19,27 +21,32 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
             Console.WriteLine($"{args} : {result}");
         }
+        //
+        //method which return required digit which we send to the finddigit method which is in string form
+        // 
+        public static  string actualdigit(string z,string a)
+        {
+            for(int index=0;index<a.Length;index++)
+                {
+                    for(int zindex=0;zindex<z.Length;zindex++)
+                    {
+                        if(z[zindex]==a[index])
+                        {
+                            z=z.Remove(zindex,1);
+                        }
+                    }
+                }
+            return z;
+        }
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            int i,j;
+            
             string a="",b="",c="";
             //fetching all the operands from equation
-            for(i=0;i<equation.IndexOf('*');i++)
-            {
-                a=a+equation[i];
-            }
-
-            for(i=equation.IndexOf('*')+1;i<equation.IndexOf('=');i++)
-            {
-                b=b+equation[i];
-            }
-
-            for(i=equation.IndexOf('=')+1;i<equation.Length;i++)
-            {
-                c=c+equation[i];
-            }
+            a=equation.Split("*")[0];
+            b=equation.Split("*")[1].Split("=")[0];
+            c=equation.Split("=")[1];
             //
             //checking for operand 1st
             //
@@ -48,21 +55,17 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 int d=Convert.ToInt32(b);
                 int e=Convert.ToInt32(c);
                 int y=e/d;
-                if(e%d!=0)
-                return -1;
-                string z=Convert.ToString(y);
-                if(z.Length<a.Length)
-                return -1;
-                for(j=0;j<a.Length;j++)
-                {
-                    if(z.Contains(a[j]))
-                    {
-                
-                    z=z.Replace(a[j],' ');
-                    }
-                }
 
-                    return Convert.ToInt32(z);
+                if(e%d!=0)
+                    return -1;
+
+                string z=Convert.ToString(y);
+
+                if(z.Length<a.Length)
+                    return -1;
+            
+                string result=actualdigit(z,a);
+                return Convert.ToInt32(result);
             }
             //
             //checking for operand 2nd
@@ -72,20 +75,17 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 int d=Convert.ToInt32(a);
                 int e=Convert.ToInt32(c);
                 int y=e/d;
+
                 if(e%d!=0)
-                return -1;
+                    return -1;
+
                 string z=Convert.ToString(y);
                 
                 if(z.Length<b.Length)
-                return -1;
-                for(j=0;j<b.Length;j++)
-                {
-                    if(z.Contains(b[j]))
-                     z=z.Replace(b[j],' ');
-                    
-                }
+                    return -1;
 
-                 return Convert.ToInt32(z);
+                string result=actualdigit(z,b);
+                return Convert.ToInt32(result);
             }
             //
             //checking for resulant value
@@ -96,18 +96,13 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 int e=Convert.ToInt32(a);
                 int y=e*d;
                 string z=Convert.ToString(y);
-                if(z.Length<c.Length)
-                return -1;
-                for(j=0;j<c.Length;j++)
-                {
-                    if(z.Contains(c[j]))
-                     z=z.Replace(c[j],' ');
-                    
-                }
                 
-                    return Convert.ToInt32(z);
-            }
-        
+                if(z.Length<c.Length)
+                    return -1;
+
+                string result=actualdigit(z,c);
+                return Convert.ToInt32(result);
+            }   
             
             throw new NotImplementedException();   
         }
